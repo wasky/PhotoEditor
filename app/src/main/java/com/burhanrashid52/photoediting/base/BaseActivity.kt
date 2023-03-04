7b84447420400764697a4902804676fd6ce974fd
Michal Wolski <wasky@users.noreply.github.com>
2023-03-04 20:37:47 +0100

    Test 7

--- app/src/main/java/com/burhanrashid52/photoediting/base/BaseActivity.kt
+++ app/src/main/java/com/burhanrashid52/photoediting/base/BaseActivity.kt
@@ -25,7 +25,7 @@ open class BaseActivity : AppCompatActivity() {
     }
 
     fun requestPermission(permission: String): Boolean {
-        // Test 6
+        // Test 7
         val isGranted = ContextCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_GRANTED
         if (!isGranted) {
             mPermission = permission
